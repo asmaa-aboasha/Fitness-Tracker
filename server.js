82d87638d7c66b555782fa9cba7bf0429cf8dc6a
Asmaa <asmaa.aboasha@gmail.com>
2020-11-21 14:49:24 -0800

    Set up server and mongo connection

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const logger = require("morgan");
+const mongoose = require("mongoose");
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(logger("dev"));
+
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Routes
+app.use(require("./routes/apiRoutes"));
+app.use(require("./routes/htmlRoutes"));
+
+// Mongoose connection
+mongoose.connect(
+  process.env.MONGODB_URI || "mongodb://localhost/workout",
+  { useNewUrlParser: true }
+);
+
+// PORT
+app.listen(PORT, function() {
+  console.log(`Now listening on port: ${PORT}`);
+});
