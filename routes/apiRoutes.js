66e4d25bf04ea4f0311da0b1565e5569c5dd7582
Asmaa <asmaa.aboasha@gmail.com>
2020-11-21 15:17:31 -0800

    Added api routes

---
+++ routes/apiRoutes.js
@@ -0,0 +1,34 @@
+const router = require("express").Router();
+const Workout = require("../models/workout.js");
+
+router.get("/api/workouts", (req, res) => {
+  Workout.find()
+    .then((dbWorkout) => {
+      res.json(dbWorkout);
+    })
+    .catch((err) => {
+      res.json(err);
+    });
+});
+
+router.post("/api/workouts", (req, res) => {
+    Workout.create({})
+      .then(dbWorkout => {
+        res.json(dbWorkout);
+      })
+      .catch((err) => {
+        res.json(err);
+      });
+  });
+
+router.get("/api/workouts/range", (req, res) => {
+    Workout.find({})
+        .then(dbWorkout => {
+        res.json(dbWorkout);
+    })
+    .catch((err) => {
+        res.json(err);
+      });
+  });
+
+module.exports = router;
