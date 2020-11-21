99e905465a027397f102d5cff853c8c64fe5d808
Asmaa <asmaa.aboasha@gmail.com>
2020-11-21 14:02:19 -0800

    Added workout.js in models

---
+++ models/workout.js
@@ -0,0 +1,43 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const workoutSchema = new Schema({
+    day: {
+       type: Date,
+       default: Date.now
+    },
+    exercises: [{
+        type: {
+            type: String,
+            required: true
+        },
+        name: {
+            type: String,
+            required: true
+        },
+        duration: {
+            type: String,
+            required: true
+        },
+        weight: {
+            type: Number,
+            required: true
+        },
+        reps: {
+            type: Number,
+            required: true
+        },
+        sets: {
+            type: Number,
+            required: true
+        },
+        distance: {
+            type: Number,
+            required: true
+        }
+    }]
+});
+
+const Workout = mongoose.model("Workout", workoutSchema);
+
+module.exports = Workout;
